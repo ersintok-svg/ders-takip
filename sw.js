6de70252e14bcff357fd2b38d77626f605df1138
ersin <tokersin@gmail.com>
2026-07-30 20:47:56 +0300

    safari cache fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE='ders-takip-v1';
+var CACHE='ders-takip-v2';
 var FILES=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}))});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(n){return n!==CACHE}).map(function(n){return caches.delete(n)}))}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}))});
+self.addEventListener('activate',function(e){e.waitUntil(clients.claim());e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(n){return n!==CACHE}).map(function(n){return caches.delete(n)}))}))});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(function(r){return r||fetch(e.request)}))});
